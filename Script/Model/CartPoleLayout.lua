23fdf9183ed1483eaef0f46356a5295835a35819
xingfanou <757609414@qq.com>
2021-05-26 13:24:09 +0800

    完成DQN，带杆小车强化学习

--- Script/Model/CartPoleLayout.lua
+++ Script/Model/CartPoleLayout.lua
@@ -7,31 +7,7 @@ local ALittle = ALittle
 local ___rawset = rawset
 local ___pairs = pairs
 local ___ipairs = ipairs
-local ___all_struct = ALittle.GetAllStruct()
 
-ALittle.RegStruct(-1479093282, "ALittle.UIEvent", {
-name = "ALittle.UIEvent", ns_name = "ALittle", rl_name = "UIEvent", hash_code = -1479093282,
-name_list = {"target"},
-type_list = {"ALittle.DisplayObject"},
-option_map = {}
-})
-ALittle.RegStruct(958494922, "ALittle.UIChangedEvent", {
-name = "ALittle.UIChangedEvent", ns_name = "ALittle", rl_name = "UIChangedEvent", hash_code = 958494922,
-name_list = {"target"},
-type_list = {"ALittle.DisplayObject"},
-option_map = {}
-})
-
-assert(ADeeplearning.CommonTrainLayout, " extends class:ADeeplearning.CommonTrainLayout is nil")
-ADeeplearning.CartPoleTrainLayout = Lua.Class(ADeeplearning.CommonTrainLayout, "ADeeplearning.CartPoleTrainLayout")
-
-function ADeeplearning.CartPoleTrainLayout.__getter:model()
-	if self._model == nil then
-		self._model = deeplearning.DeeplearningDQNModel()
-		self._model_path = ADeeplearning.g_ModuleBasePath .. "Other/cartpole.model"
-	end
-	return self._model
-end
 
 ADeeplearning.CartPoleAction = {
 	LEFT = 0,
@@ -47,7 +23,7 @@ function ADeeplearning.CartPoleLayout:Ctor()
 	___rawset(self, "_masscart", 1.0)
 	___rawset(self, "_masspole", 0.5)
 	___rawset(self, "_total_mass", 0)
-	___rawset(self, "_pole_length", 4)
+	___rawset(self, "_pole_length", 5)
 	___rawset(self, "_polemass_length", 0)
 	___rawset(self, "_force_mag", 10.0)
 	___rawset(self, "_action", 0)
@@ -55,18 +31,16 @@ function ADeeplearning.CartPoleLayout:Ctor()
 	___rawset(self, "_cart_x_dot", 0)
 	___rawset(self, "_pole_theta", 0)
 	___rawset(self, "_pole_theta_dot", 0)
+	___rawset(self, "_theta_threshold_radians", 1.570796325)
+	___rawset(self, "_x_threshold", 5)
 end
 
 function ADeeplearning.CartPoleLayout:TCtor()
-	self._train:AddEventListener(___all_struct[958494922], self, self.HandleTrainChanged)
-	self._model_path = ADeeplearning.g_ModuleBasePath .. "Other/mnist.model"
-	self._model = deeplearning.DeeplearningMnistModel()
+	self._model_path = ADeeplearning.g_ModuleBasePath .. "Other/cartpole.model"
+	self._model = deeplearning.DeeplearningDQNModel(4, 2, 100, 2000)
 	self._total_mass = self._masscart + self._masspole
 	self._polemass_length = self._masspole * self._polemass_length
-end
-
-function ADeeplearning.CartPoleLayout:HandleTrainChanged(event)
-	self._model:Load(self._model_path)
+	self:GameInit(0)
 end
 
 function ADeeplearning.CartPoleLayout:HandleStartGameClick(event)
@@ -74,13 +48,25 @@ function ADeeplearning.CartPoleLayout:HandleStartGameClick(event)
 		self._model:Load(self._model_path)
 		self._loaded = true
 	end
-	self:GameInit(ALittle.Math_RandomDouble(-10, 10))
+	self:GameInit(0)
 	if self._loop_frame ~= nil then
 		self._loop_frame:Stop()
 	end
 	self._loop_frame = ALittle.LoopFrame(Lua.Bind(self.HandleFrame, self))
 	self._loop_frame:Start()
 	self._start_button.disabled = true
+end
+
+function ADeeplearning.CartPoleLayout:HandleStopGameClick(event)
+	if self._loaded then
+		self._model:Save(self._model_path)
+	end
+	self:GameInit(0)
+	if self._loop_frame ~= nil then
+		self._loop_frame:Stop()
+		self._loop_frame = nil
+	end
+	self._start_button.disabled = false
 end
 
 function ADeeplearning.CartPoleLayout:GameInit(angle)
@@ -95,6 +81,11 @@ function ADeeplearning.CartPoleLayout:GameInit(angle)
 end
 
 function ADeeplearning.CartPoleLayout:HandleFrame(frame_time)
+	local state = {}
+	state[1] = self._cart_x
+	state[2] = self._cart_x_dot
+	state[3] = self._pole_theta
+	state[4] = self._pole_theta_dot
 	local action = self._action
 	if self._self_play.selected then
 		if A_UISystem.sym_map[1073741904] ~= nil then
@@ -103,14 +94,32 @@ function ADeeplearning.CartPoleLayout:HandleFrame(frame_time)
 			action = 1
 		end
 	else
+		action = self._model:ChooseAction(state, 9)
 	end
-	local x, degree, done = self:GameRun(action, frame_time)
+	local done = self:GameRun(action, frame_time)
+	local next_state = {}
+	next_state[1] = self._cart_x
+	next_state[2] = self._cart_x_dot
+	next_state[3] = self._pole_theta
+	next_state[4] = self._pole_theta_dot
 	if done then
 		if self._loop_frame ~= nil then
 			self._loop_frame:Stop()
 			self._loop_frame = nil
 		end
+		self:GameInit(0)
+		if self._loop_frame ~= nil then
+			self._loop_frame:Stop()
+		end
+		self._loop_frame = ALittle.LoopFrame(Lua.Bind(self.HandleFrame, self))
+		self._loop_frame:Start()
 		self._start_button.disabled = false
+	else
+		local r1 = (self._x_threshold - ALittle.Math_Abs(self._cart_x)) / self._x_threshold - 0.8
+		local r2 = (self._theta_threshold_radians - ALittle.Math_Abs(self._pole_theta)) / self._theta_threshold_radians - 0.5
+		local reward = r1 + r2
+		self._model:SaveTransition(state, action, reward, next_state)
+		self._model:Learn()
 	end
 end
 
@@ -134,15 +143,13 @@ function ADeeplearning.CartPoleLayout:GameRun(action, frame_time)
 	x_dot = x_dot + tau * xacc
 	theta = theta + tau * theta_dot
 	theta_dot = theta_dot + tau * thetaacc
-	local theta_threshold_radians = 90 * 2 * 3.14159265 / 360
-	local x_threshold = 3
-	local done = x < -x_threshold or x > x_threshold or theta < -theta_threshold_radians or theta > theta_threshold_radians
+	local done = x < -self._x_threshold or x > self._x_threshold or theta < -self._theta_threshold_radians or theta > self._theta_threshold_radians
 	self._cart_x = x
 	self._cart_x_dot = x_dot
 	self._pole_theta = theta
 	self._pole_theta_dot = theta_dot
 	self:GameRender()
-	return self._cart_x, self._pole_theta, done
+	return done
 end
 
 function ADeeplearning.CartPoleLayout:GameRender()
